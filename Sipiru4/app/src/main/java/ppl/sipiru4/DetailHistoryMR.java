90fc7db490b9a191f90d46c94323bed56d7f4ccf
Gina Andriyani <gina.andriyani.wantania@gmail.com>
2015-05-24 10:26:16 +0700

    implementasi untuk mengunduh bukti peminjaman oleh manajer ruangan

--- Sipiru4/app/src/main/java/ppl/sipiru4/DetailHistoryMR.java
+++ Sipiru4/app/src/main/java/ppl/sipiru4/DetailHistoryMR.java
@@ -12,11 +12,27 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.renderscript.Element;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.lowagie.text.Cell;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.HeaderFooter;
+import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Table;
+import com.lowagie.text.pdf.PdfFormField;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPCellEvent;
+import com.lowagie.text.pdf.PdfTable;
+import com.lowagie.text.pdf.PdfWriter;
 
 import org.json.JSONArray;
 
@@ -29,6 +45,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ppl.sipiru4.Entity.JSONParser;
 import ppl.sipiru4.Entity.Peminjaman;
@@ -43,6 +62,13 @@ public class DetailHistoryMR extends Activity {
     // Progress Dialog Object
     private ProgressDialog pDialog;
 
+    //untuk nulis ke PDF
+    String namaPeminjam = "";
+    String ruangDipinjam = "";
+    String tglPinjam = "";
+    String peralatan ="";
+    String keperluan="";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_history_mr);
@@ -54,6 +80,13 @@ public class DetailHistoryMR extends Activity {
 
         Log.e("peminjaman", peminjamanController.getPeminjaman().getKodeRuangan() + " " + peminjamanController.getPeminjaman().getNamaP()
                 + " " + peminjamanController.getPeminjaman().getId());
+
+        //untuk bikin file PDF
+        namaPeminjam = peminjamanController.getPeminjaman().getNamaP();
+        ruangDipinjam = peminjamanController.getPeminjaman().getKodeRuangan();
+        tglPinjam = peminjamanController.getPeminjaman().getMulai() + peminjamanController.getPeminjaman().getSelesai();
+        peralatan = peminjamanController.getPeminjaman().getPeralatan();
+        keperluan = peminjamanController.getPeminjaman().getPerihal() + ", "+ peminjamanController.getPeminjaman().getKegiatan();
 
         TextView ruang = (TextView)findViewById(R.id.ruang);
         ruang.setText(peminjamanController.getPeminjaman().getKodeRuangan());
@@ -83,15 +116,17 @@ public class DetailHistoryMR extends Activity {
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File file = new File(Environment.getExternalStorageDirectory().getPath()+"/jai_ho.mp3");
-                // Check if the Music file already exists
-                if (file.exists()) {
-                    Toast.makeText(getApplicationContext(), "File already exist under SD card", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "File doesn't exist under SD Card, downloading Mp3 from Internet", Toast.LENGTH_LONG).show();
-                    // Trigger Async Task (onPreExecute method)
-                    new DownloadFileFromURL().execute(file_url);
-                }
+                createPDF();
+                openPdf();
+//                File file = new File(Environment.getExternalStorageDirectory().getPath()+"/jai_ho.mp3");
+//                // Check if the Music file already exists
+//                if (file.exists()) {
+//                    Toast.makeText(getApplicationContext(), "File already exist under SD card", Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "File doesn't exist under SD Card, downloading Mp3 from Internet", Toast.LENGTH_LONG).show();
+//                    // Trigger Async Task (onPreExecute method)
+//                    new DownloadFileFromURL().execute(file_url);
+//                }
             }
         });
     }
@@ -207,87 +242,166 @@ public class DetailHistoryMR extends Activity {
 //            startActivity(i);
         }
     }
-//    public void createPDF()
-//    {
-//        Document doc = new Document();
+    void createPDF()
+    {
+        Document doc = new Document();
+
+        try {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/BuktiPeminjaman";
+
+            File dir = new File(path);
+            if(!dir.exists())
+                dir.mkdirs();
+
+            Log.d("PDFCreator", "PDF Path: " + path);
+
+            File file = new File(dir,  namaPeminjam + "_"+ ruangDipinjam + "_" + ".pdf");
+            if(file.exists()){
+                file.delete();
+                file.createNewFile();
+            }
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            PdfWriter.getInstance(doc, fOut);
+
+            //open the document
+            doc.open();
+
+   /* Create Paragraph and Set Font */
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            Date date = new Date();
+            Paragraph tgl = new Paragraph(dateFormat.format(date));
+            Paragraph judul = new Paragraph("BUKTI PEMINJAMAN\n" + "(BARANG & RUANGAN)");
+
+            Font fontjudulP= new Font(Font.HELVETICA);
+            fontjudulP.setSize(40);
+
+            Paragraph judulP = new Paragraph("SURAT PERNYATAAN");
+            Paragraph pernyataan = new Paragraph("Dengan surat ini saya mengatakan, untuk menajaga kebersihan dan " +
+                    "kerapihan ruangan selama peminjaman. Apabila dikemudian hari" +
+                    "saya melanggar, saya akan menerima segala konsekuensinya yang diberikan dari pihak Fakultas." +
+                    "Demikian surat pernyataan ini saya buat dengan sebaik baiknya. \n\n\n");
+            pernyataan.getExtraParagraphSpace();
+
+            Font paraFont= new Font(Font.HELVETICA);
+            paraFont.setSize(20);
+            tgl.setAlignment(Paragraph.ALIGN_RIGHT);
+            tgl.setFont(paraFont);
+
+            judul.setAlignment(Paragraph.ALIGN_CENTER);
+            judul.setFont(fontjudulP);
+
+            judulP.setAlignment(Paragraph.ALIGN_CENTER);
+            judulP.setFont(fontjudulP);
+
+            pernyataan.setAlignment(Paragraph.ALIGN_LEFT);
+            pernyataan.setFont(paraFont);
+
+            Table ttd = new Table(2);
+            ttd.setAlignment(com.lowagie.text.Element.ALIGN_CENTER);
+            ttd.addCell("Yang Menerima\n\n\n\n");
+            ttd.addCell("Yang Menyerahkan\n\n\n\n");
+            ttd.addCell(namaPeminjam);
+            ttd.addCell("Harry Sunarto");
+
+
+
+//            Paragraph judulttd = new Paragraph("Yang Menerima                                                                                            ");
+//            judulttd.setAlignment(Paragraph.ALIGN_LEFT);
+//            judulttd.setSpacingAfter(20);
+//            judulttd.add("Yang Menyerahkan\n\n\n");
 //
-//        try {
-//            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/PDF";
+//            Paragraph namattd = new Paragraph("("+namaPeminjam+ ")" + "                                                                                            ");
+//            namattd.setAlignment(Paragraph.ALIGN_LEFT);
+//            namattd.setSpacingAfter(20);
+//            namattd.add("(" + "Harry Sunarto" + ")");
+
+
+
+
+//            doc.addTitle("Bukti Peminjaman\n" + "Barang & Ruangan");
+            Table tabel = new Table(2);
+            tabel.setAlignment(Paragraph.ALIGN_MIDDLE);
+            tabel.addCell("Nama : ");
+            tabel.addCell(namaPeminjam);
+            tabel.addCell("Nama Ruangan :");
+            tabel.addCell(ruangDipinjam);
+            tabel.addCell("Tgl Peminjaman ");
+            tabel.addCell(tglPinjam);
+            tabel.addCell("Nama Barang :");
+            if(peralatan.equalsIgnoreCase("")){
+                tabel.addCell("-");
+            }
+            else{
+                tabel.addCell(peralatan);
+            }
+            tabel.addCell("Keperluan :");
+            tabel.addCell(keperluan);
+
+            doc.add(tgl);
+            doc.add(judul);
+            doc.add(tabel);
+            doc.newPage();
+            doc.add(judulP);
+            doc.add(pernyataan);
+            doc.add(ttd);
+//            doc.add(judulttd);
+//            doc.add(namattd);
 //
-//            File dir = new File(path);
-//            if(!dir.exists())
-//                dir.mkdirs();
-//
-//            Log.d("PDFCreator", "PDF Path: " + path);
-//
-//            File file = new File(dir, "demo.pdf");
-//            FileOutputStream fOut = new FileOutputStream(file);
-//
-//            PdfWriter.getInstance(doc, fOut);
-//
-//            //open the document
-//            doc.open();
-//
-//   /* Create Paragraph and Set Font */
-//            Paragraph p1 = new Paragraph("Hi! I am Generating my first PDF using DroidText");
-//
-//   /* Create Set Font and its Size */
-//            Font paraFont= new Font(Font.HELVETICA);
-//            paraFont.setSize(16);
-//            p1.setAlignment(Paragraph.ALIGN_CENTER);
-//            p1.setFont(paraFont);
-//
+
+//            Paragraph p2 = new Paragraph("Ruangan : " + ruangDipinjam);
+//            Paragraph p3 = new Paragraph("Tgl Peminjaman :" + tglPinjam);
+
+
+   /* Create Set Font and its Size */
+
+
+//            p2.setAlignment(Paragraph.ALIGN_CENTER);
+//            p2.setFont(paraFont);
 //            //add paragraph to document
 //            doc.add(p1);
-//
-//
-//            Paragraph p2 = new Paragraph("This is an example of a simple paragraph");
-//
-//  /* You can also SET FONT and SIZE like this */
-//            Font paraFont2= new Font(Font.COURIER,14.0f, Color.GREEN);
-//            p2.setAlignment(Paragraph.ALIGN_CENTER);
-//            p2.setFont(paraFont2);
-//
 //            doc.add(p2);
-//
-//   /* Inserting Image in PDF */
+
+
+
+   /* Inserting Image in PDF */
 //            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//            Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.android);
+//            Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.ikon_sipiru);
 //            bitmap.compress(Bitmap.CompressFormat.JPEG, 100 , stream);
 //            Image myImg = Image.getInstance(stream.toByteArray());
 //            myImg.setAlignment(Image.MIDDLE);
 //
 //            //add image to document
 //            doc.add(myImg);
-//
-//            //set footer
-//            Phrase footerText = new Phrase("This is an example of a footer");
-//            HeaderFooter pdfFooter = new HeaderFooter(footerText, false);
-//            doc.setFooter(pdfFooter);
-//
-//            Toast.makeText(getApplicationContext(), "Created...", Toast.LENGTH_LONG).show();
-//
-//        } catch (DocumentException de) {
-//            Log.e("PDFCreator", "DocumentException:" + de);
-//        } catch (IOException e) {
-//            Log.e("PDFCreator", "ioException:" + e);
-//        }
-//        finally
-//        {
-//            doc.close();
-//        }
-//    }
-//
-//    void openPdf()
-//    {
-//        Intent intent = new Intent(Intent.ACTION_VIEW);
-//        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/PDF";
-//
-//        File file = new File(path, "demo.pdf");
-//
-//        intent.setDataAndType( Uri.fromFile(file), "application/pdf" );
-//        startActivity(intent);
-//    }
+
+            //set footer
+            Phrase footerText = new Phrase("This is an example of a footer");
+            HeaderFooter pdfFooter = new HeaderFooter(footerText, false);
+            doc.setFooter(pdfFooter);
+
+            Toast.makeText(getApplicationContext(), "Created...", Toast.LENGTH_LONG).show();
+
+        } catch (DocumentException de) {
+            Log.e("PDFCreator", "DocumentException:" + de);
+        } catch (IOException e) {
+            Log.e("PDFCreator", "ioException:" + e);
+        }
+        finally
+        {
+            doc.close();
+        }
+    }
+
+    void openPdf()
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/BuktiPeminjaman";
+
+        File file = new File(path, namaPeminjam + "_"+ ruangDipinjam + "_" + ".pdf");
+
+        intent.setDataAndType( Uri.fromFile(file), "application/pdf" );
+        startActivity(intent);
+    }
 }
 
 
